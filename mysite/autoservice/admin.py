6cfb37fb0babdf167669a3ed96dcd42d5bbba43a
DonatasNoreika <donoras@gmail.com>
2026-04-14 18:54:53 +0300

    1: + Admin puslapyje sukurti meniu punktus visiems sukurtiems modeliams

--- mysite/autoservice/admin.py
+++ mysite/autoservice/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Service, Car, Order, OrderLine
+
+class OrderLineInLine(admin.TabularInline):
+    model = OrderLine
+    extra = 0
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['car', 'date', 'client', 'status']
+    inlines = [OrderLineInLine]
 
 # Register your models here.
+admin.site.register(Service)
+admin.site.register(Car)
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderLine)
